c4939ad2db73309d208588c6c566cd526dbc0939
Tntu06 <bongdem20042006@gmail.com>
2025-12-27 19:37:51 +0700

    Thay đổi tên Laptop

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ import pyodbc
 def get_connection():
     return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=LAPTOP-RIA3KGI9;"
+        "SERVER=LAPTOP-ACER;"
         "DATABASE=QUANLYBANHANG;"
         "Trusted_Connection=yes;"
         "TrustServerCertificate=yes;"
     )
+
