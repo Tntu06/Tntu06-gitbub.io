8ba003f66deb358653335e9945709758a9f1f480
T-TÚ <bongdem20042006@gmail.com>
2026-01-05 18:41:19 +0700

    Update database.py
    Change the connect the database

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ import pyodbc
 
 def get_connection():
     return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=LAPTOP-ACER;"
+        "DRIVER={......};"
+        "SERVER=.....;"
         "DATABASE=QUANLYBANHANG;"
         "Trusted_Connection=yes;"
         "TrustServerCertificate=yes;"
     )
+
 
